2f8e7ada9b748d4883e4ae715fb0d37809498569
Pawel Kuncicki <gebruiker@gebruikers-MacBook-Air.local>
2020-06-03 07:44:41 +0200

    adding firebase config

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });

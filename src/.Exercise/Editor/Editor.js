79ef0a0f659beea794179b48581f2b4bfbf89a78
Pawel Kuncicki <pawel.kuncicki@gmail.com>
2020-09-14 17:01:47 +0200

    Editor changes - removing redundunt items

--- src/.Exercise/Editor/Editor.js
+++ src/.Exercise/Editor/Editor.js
@@ -16,9 +16,9 @@ const FloovioEditor = (props) => {
                 width: 979,
                 skin: "material-outline",
                 statusbar: false,
-                plugins: 'image imagetools table lists template paste',
-                menubar: 'edit view insert table',
-                toolbar: "undo redo | bold italic underline strikethrough | numlist bullist | outdent indent | forecolor removeformat | alignleft aligncenter alignright alignjustify | formatselect | image template",
+                plugins: 'lists template paste',
+                menubar: 'edit insert',
+                toolbar: "undo redo | bold italic underline strikethrough | numlist bullist | outdent indent | forecolor removeformat | alignleft aligncenter alignright alignjustify | formatselect | template",
                 templates: [
                     {title: 'Image', description: '<img,8chrcode,200>'},
                     {title: 'Fill the gaps', description: 'Fill the gaps type exercise, based on a real exercise from Spanish', content: '<p><span style="color: #ba372a;"><strong>Escoja la opci&oacute;n correcta para completar las siguientes oraciones.</strong></span></p><ol><li style="font-weight: 400;">El doctor le dar&aacute; una _receta_ para comprar el medicamento en la farmacia.&nbsp;</li><li style="font-weight: 400;">Tiene que _pedir cita_ para que el doctor le atienda.&nbsp;</li><li style="font-weight: 400;">Siente mucho calor y est&aacute; sudando porque tiene mucha _fiebre_.</li><li style="font-weight: 400;">La _consulta_ del doctor Aguado empieza a las diez de la ma&ntilde;ana.&nbsp;</li><li style="font-weight: 400;">El doctor Aguado tiene muchos _pacientes_ ;la _sala de espera_ siempre est&aacute; llena.&nbsp;</li><li style="font-weight: 400;">Esta noche no pod&iacute;a dormir porque ten&iacute;a mucha _tos_&nbsp;</li><li style="font-weight: 400;">Pablo se cuida mucho: lleva una _vida saluda_</li><li style="font-weight: 400;">En el hospital la enfermera me _toma la tension_ cada ma&ntilde;ana.</li></ol>'},

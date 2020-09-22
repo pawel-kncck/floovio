95b762f08637cd1e624948587bc5a5d359687746
Pawel Kuncicki <pawel.kuncicki@gmail.com>
2020-09-22 10:37:36 +0200

    adding table templates

--- src/.Exercise/Editor/Editor.js
+++ src/.Exercise/Editor/Editor.js
@@ -4,22 +4,25 @@ import { stripTags } from '../../.Utilities/Utilities';
 import parser from '../functions/parser';
 import { updateFloovio } from '../../.Store/floovio.actions';
 import { connect } from 'react-redux';
+import floovioTemplates from './templates.json';
 
 const FloovioEditor = (props) => {
 
     return (
         <Editor
             apiKey="i36gbjqh8ydba7ux5zg956l58ckmicfaa8rib2yuv9d3agu6"
-            initialValue={props.html || '' }
+            initialValue={props.html || ''}
             init={{
                 height: 450,
                 width: 979,
                 skin: "material-outline",
-                statusbar: true,
+                statusbar: false,
                 plugins: 'table lists template paste',
-                menubar: 'edit insert',
+                menubar: 'edit',
                 table_grid: false,
-                toolbar: "undo redo | bold italic underline strikethrough | numlist bullist | outdent indent | forecolor removeformat | alignleft aligncenter alignright alignjustify | formatselect | template",
+                table_toolbar: '',
+                toolbar: "undo redo | bold italic underline strikethrough | numlist bullist | table template | forecolor removeformat | alignleft aligncenter alignright alignjustify | formatselect",
+                templates: floovioTemplates,
                 paste_preprocess: (plugin, args) => {
                     args.content = stripTags( args.content,'li,ol' )
                 },  
